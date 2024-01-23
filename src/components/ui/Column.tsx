65800237d652375da80e69ec4d5b149b7a0f7017
Rami Bitar <rbitar@gmail.com>
2024-01-23 13:19:32 -0500

    Update Column

--- src/components/ui/Column.tsx
+++ src/components/ui/Column.tsx
@@ -4,16 +4,26 @@ import { Container } from '@mui/material'
 type ColumnProps = {
 	children: React.ReactNode
 	bgcolor?: string
+  py?: number
+  gap?: number 
+  cols?: number
 	maxWidth?: 'xs' | 'sm' | 'md' | 'lg' | 'xl'
 }
 
 const Column: React.FC<ColumnProps> = (props) => {
-	const { children, bgcolor, maxWidth } = props
+	const { children, cols=1, gap=0, py=4, bgcolor, maxWidth } = props
 
 	return (
 		<Container
 			sx={{
-				...sx.root,
+				...sx.root,        
+        gridTemplateColumns: { 
+          sm: `repeat(${Number(cols)}, 1fr)`, 
+          xs: '1fr'
+        },
+        gap,
+        py,
+        px: py > 0 ? 2 : 0,
 				bgcolor,
 			}}
 			maxWidth={maxWidth}
